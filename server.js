b2059867b025d0810be18ac25ced5bdc09b4fb62
Felipe Ricardo de Sousa <fee.sousa09@gmail.com>
2026-04-07 15:49:40 -0300

    criação da api básica

---
+++ server.js
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+
+const servidor = Fastify();
+
+//-----------------------------------------------------------------------------
+
+servidor.get('/produtos', async (request, reply) => {
+    return reply.status(200).send({id: 1, nome: 'Produto 1', preco: 10.99});
+});
+
+servidor.post('/produtos', async (request, reply) => {
+    const tema = request.body.tema;
+    if (!tema) {
+        return reply.status(400).send({error: 'O campo "tema" é obrigatório.'});
+    };
+    return reply.status(200).send({id: 1, nome: `Produto relacionado a ${tema}`, preco: 10.99});
+});
+
+//-----------------------------------------------------------------------------
+
+servidor.listen({
+  port: process.env.PORT || 3000,
+  host: '0.0.0.0'
+});
